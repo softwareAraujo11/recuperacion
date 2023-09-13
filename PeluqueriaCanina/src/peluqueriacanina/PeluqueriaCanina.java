06fcc6d05b563d0f252c0b56b067af567dc48fd0
softwareAraujo11 <softwareengineer.felipearaujo@gmail.com>
2023-09-13 13:16:08 -0500

    recuperacion

---
+++ PeluqueriaCanina/src/peluqueriacanina/PeluqueriaCanina.java
@@ -0,0 +1,15 @@
+package peluqueriacanina;
+
+import peluqueriacanina.igu.CargaDatos;
+import peluqueriacanina.igu.Principal;
+
+public class PeluqueriaCanina {
+
+    public static void main(String[] args) {
+
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+        
+    }
+}
---
+++ PeluqueriaCanina/src/peluqueriacanina/logica/Mascota.java
@@ -0,0 +1,17 @@
+
+package peluqueriacanina.logica;
+
+public class Mascota {
+    
+    private int num_Cliente;
+    private String nombre;
+    private String raza;
+    private String color;
+    private String alergico;
+    private String atencion_especial;
+    private String observaciones;
+    
+    
+    
+    
+}
